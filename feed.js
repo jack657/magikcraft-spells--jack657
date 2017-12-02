42b8d4dccc5e9f0469f4ea6390d10f3ff6b10d89
jack657 <jennylockk@gmail.com>
2017-12-02 11:45:47 +0000

    Update feed.js

--- feed.js
+++ feed.js
@@ -2,4 +2,11 @@ const magik = magikcraft.io;
 
 function feed() {
 	magik.satio();
+    magik.satio();
+    magik.satio();
+    magik.satio();
+    magik.satio();
+    magik.satio();
+    magik.satio();
+    
 }
